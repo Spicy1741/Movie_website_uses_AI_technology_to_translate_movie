650d567510c2805a5c7482a8df7cf128c57a34ef
Wistoria04 <thomas300304h@gmail.com>
2025-06-28 21:59:28 +0700

    update subtitle movie

---
+++ Migrations/20250628091910_ReAddMovieTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Film_website.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReAddMovieTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SubtitlePath",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubtitlePath",
+                table: "Movies");
+        }
+    }
+}
